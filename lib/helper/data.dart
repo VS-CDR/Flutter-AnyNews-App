28fa48cf29563369792358ba01eb548a7e55e132
Vlad Shubin <shubin.va@phystech.edu>
2024-03-04 11:11:32 +0300

    base appbar app

---
+++ lib/helper/data.dart
@@ -0,0 +1,27 @@
+import '../models/category_model.dart';
+
+List<CategoryModel> getCategories() {
+  List<CategoryModel> categories = List<CategoryModel>.empty(growable: true);
+  CategoryModel categoryModel = CategoryModel();
+
+  categoryModel.categoryName = "Economics";
+  categoryModel.imgUrl = 'assets/news_categories/ibrahim-boran-_aUwE2DnIPg-unsplash.jpg';
+  categories.add(categoryModel);
+
+  categoryModel = CategoryModel();
+  categoryModel.categoryName = "Politics";
+  categoryModel.imgUrl = 'assets/news_categories/marco-oriolesi-wqLGlhjr6Og-unsplash.jpg';
+  categories.add(categoryModel);
+
+  categoryModel = CategoryModel();
+  categoryModel.categoryName = "Sports";
+  categoryModel.imgUrl = 'assets/news_categories/mario-klassen-70YxSTWa2Zw-unsplash.jpg';
+  categories.add(categoryModel);
+
+  categoryModel = CategoryModel();
+  categoryModel.categoryName = "Science";
+  categoryModel.imgUrl = 'assets/news_categories/ousa-chea-gKUC4TMhOiY-unsplash.jpg';
+  categories.add(categoryModel);
+
+  return categories;
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:any_news/views/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter HW',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      useMaterial3: true,
+    ),
+    home: const Home(),
+  );
+}
